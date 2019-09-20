9e5a9e83eb9bf264cb12599696a1850281b6d5fa
Werstern <wersternn@gmail.com>
2019-09-20 17:26:12 +0300

    add some styles

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -3,6 +3,7 @@ import { View, Text, Button, Image, StyleSheet } from 'react-native';
 
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
+import Colors from '../constants/colors';
 
 const GameOverScreen = props => {
   return (
@@ -10,12 +11,16 @@ const GameOverScreen = props => {
       <TitleText>The Game is Over!</TitleText>
       <View style={styles.imageContainer}>
         <Image 
-          source={require('../assets/success.png')} 
+          source={require('../assets/success.png')}
+          // source={{uri: 'https://cdn.theatlantic.com/assets/media/img/photo/2019/04/a-photo-visit-to-mount-fanjing/f01_1064359636-1/original.jpg'}} 
           style={styles.image} 
         />
       </View>
-      <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
-      <BodyText>Number was: {props.userNumber}</BodyText>
+      <View style={styles.resultContainer}>
+        <BodyText style={styles.resultText}>Your phone needed <Text style={styles.highlight}>
+          {props.roundsNumber}</Text> rounds to guess the number <Text style={styles.highlight}>{props.userNumber}</Text>
+        </BodyText>
+      </View>
       <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
   )
@@ -39,6 +44,18 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: '100%'
+  },
+  resultContainer: {
+    marginHorizontal: 30,
+    marginVertical: 15
+  },
+  resultText: {
+    textAlign: 'center',
+    fontSize: 20
+  },
+  highlight: {
+    color: Colors.primary,
+    fontFamily: 'open-sans-bold'
   }
 });
 

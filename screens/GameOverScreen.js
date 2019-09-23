12474d3197a5dc2ec7d6597be3453263feed9430
Werstern <wersternn@gmail.com>
2019-09-23 16:09:21 +0300

    add landscape mode

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { View, Text, Button, Image, StyleSheet } from 'react-native';
+import { 
+  View, 
+  Text, 
+  Button, 
+  Image, 
+  StyleSheet, 
+  Dimensions,
+  ScrollView 
+} from 'react-native';
 
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
@@ -8,22 +16,24 @@ import Colors from '../constants/colors';
 
 const GameOverScreen = props => {
   return (
-    <View style={styles.screen}>
-      <TitleText>The Game is Over!</TitleText>
-      <View style={styles.imageContainer}>
-        <Image 
-          source={require('../assets/success.png')}
-          // source={{uri: 'https://cdn.theatlantic.com/assets/media/img/photo/2019/04/a-photo-visit-to-mount-fanjing/f01_1064359636-1/original.jpg'}} 
-          style={styles.image} 
-        />
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText>The Game is Over!</TitleText>
+        <View style={styles.imageContainer}>
+          <Image 
+            source={require('../assets/success.png')}
+            // source={{uri: 'https://cdn.theatlantic.com/assets/media/img/photo/2019/04/a-photo-visit-to-mount-fanjing/f01_1064359636-1/original.jpg'}} 
+            style={styles.image} 
+          />
+        </View>
+        <View style={styles.resultContainer}>
+          <BodyText style={styles.resultText}>Your phone needed <Text style={styles.highlight}>
+            {props.roundsNumber}</Text> rounds to guess the number <Text style={styles.highlight}>{props.userNumber}</Text>
+          </BodyText>
+        </View>
+        <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
       </View>
-      <View style={styles.resultContainer}>
-        <BodyText style={styles.resultText}>Your phone needed <Text style={styles.highlight}>
-          {props.roundsNumber}</Text> rounds to guess the number <Text style={styles.highlight}>{props.userNumber}</Text>
-        </BodyText>
-      </View>
-      <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
-    </View>
+    </ScrollView>
   )
 };
 
@@ -34,13 +44,13 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   imageContainer: {
-    width: 300,
-    height: 300,
-    borderRadius: 150,
+    width: Dimensions.get('window').width * 0.7,
+    height: Dimensions.get('window').width * 0.7,
+    borderRadius: Dimensions.get('window').width * 0.7 / 2,
     borderWidth: 3,
     borderColor: 'black',
     overflow:"hidden",
-    marginVertical: 30
+    marginVertical: Dimensions.get('window').height / 30
   },
   image: {
     width: '100%',
@@ -48,11 +58,11 @@ const styles = StyleSheet.create({
   },
   resultContainer: {
     marginHorizontal: 30,
-    marginVertical: 15
+    marginVertical: Dimensions.get('window').height / 60
   },
   resultText: {
     textAlign: 'center',
-    fontSize: 20
+    fontSize: Dimensions.get('window').height < 400 ? 16 : 20
   },
   highlight: {
     color: Colors.primary,
